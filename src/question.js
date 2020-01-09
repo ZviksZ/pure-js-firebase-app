16d0a343836ecdb2a926cf1ca39ed7d9f9876b1f
Nikita Kapshtyk <kapshtyk.n@globalsolutions.ru>
2020-01-09 16:12:42 +0300

    first commit

---
+++ src/question.js
@@ -0,0 +1,27 @@
+export class Question {
+   static create(question) {
+      return fetch('https://pure-js-app-23.firebaseio.com/questions.json', {
+         method: 'POST',
+         body: JSON.stringify(question),
+         headers: {
+            'Content-Type': 'application/json'
+         }
+      })
+         .then(response => response.json())
+         .then(response => {
+            question.id = response.name
+            return question
+         })
+         .then(addToLocalStorage)
+   }
+}
+
+function addToLocalStorage(question) {
+   const all = getQuestionsFromLocalStorage()
+   all.push(question)
+   localStorage.setItem('questions', JSON.stringify(all))
+}
+
+function getQuestionsFromLocalStorage() {
+   return JSON.parse(localStorage.getItem('questions') || '[]')
+}
